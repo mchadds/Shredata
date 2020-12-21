993bf25c66942c419da073640a2aba3d66e22ad4
mchadds <cdundassmith@gmail.com>
2020-12-21 16:06:05 -0700

    created logging.ts to log info/warning/error/debug

--- source/config/logging.ts
+++ source/config/logging.ts
@@ -1,6 +1,50 @@
 // custom logger
 
-// gives date in nice readable format
+// gives current date in nice readable format
 const getTimeStamp = (): string => {
     return new Date().toISOString();
+};
+
+// optional object parameter is in case we want to pass something in and look at it in its JSON format
+// log for info
+const info = (namespace: string, message: string, object?: any) => {
+    if (object) {
+        console.info(`[${getTimeStamp()}] [INFO] [${namespace}] ${message}`, object);
+    } else {
+        console.info(`[${getTimeStamp()}] [INFO] [${namespace}] ${message}`);
+    }
+};
+
+// log for warning
+const warn = (namespace: string, message: string, object?: any) => {
+    if (object) {
+        console.warn(`[${getTimeStamp()}] [WARN] [${namespace}] ${message}`, object);
+    } else {
+        console.warn(`[${getTimeStamp()}] [WARN] [${namespace}] ${message}`);
+    }
+};
+
+// log for errors
+const error = (namespace: string, message: string, object?: any) => {
+    if (object) {
+        console.error(`[${getTimeStamp()}] [ERROR] [${namespace}] ${message}`, object);
+    } else {
+        console.error(`[${getTimeStamp()}] [ERROR] [${namespace}] ${message}`);
+    }
+};
+
+// log for debugging
+const debug = (namespace: string, message: string, object?: any) => {
+    if (object) {
+        console.debug(`[${getTimeStamp()}] [DEBUG] [${namespace}] ${message}`, object);
+    } else {
+        console.debug(`[${getTimeStamp()}] [DEBUG] [${namespace}] ${message}`);
+    }
+};
+
+export default {
+    info,
+    warn,
+    error,
+    debug
 };
